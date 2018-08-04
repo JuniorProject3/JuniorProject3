700dc3c4db2012ac09ac61fdf1f571f45b329d1f
balint92 <kozma.balint92@gmail.com>
2018-08-04 08:08:29 +0200

    HelloGit

---
+++ src/testsecond/HelloGit.java
@@ -0,0 +1,13 @@
+package testsecond;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		String world = "Hello World";
+
+		for (int i = 0; i < world.length(); i++) {
+			System.out.print(world.charAt(i));
+		}
+	}
+
+}
